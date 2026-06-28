46947e09a7c420ba49a100342970b3dadeb8cb60
Gabriel Sbruzzi <132472378+GabrielSbruzzi@users.noreply.github.com>
2026-06-27 22:50:46 -0300

    Update test_login.py

--- pbl-12-qualidade-ci/tests/test_login.py
+++ pbl-12-qualidade-ci/tests/test_login.py
@@ -1,1 +1,10 @@
+from login import login
 
+def test_login_sucesso():
+    assert login("admin", "1234") == "Login realizado"
+
+def test_login_senha_errada():
+    assert login("admin", "errado") == "Credenciais inválidas"
+
+def test_login_campos_vazios():
+    assert login("", "") == "Campos obrigatórios"
