0f5a9b19ea6e65dcc7304f83728fafcddb48168a
Gabriel Sbruzzi <132472378+GabrielSbruzzi@users.noreply.github.com>
2026-06-27 23:07:56 -0300

    Delete pbl-12-qualidade-ci directory

--- pbl-12-qualidade-ci/login.py
+++
@@ -1,6 +0,0 @@
-def login(usuario, senha):
-    if not usuario or not senha:
-        return "Campos obrigatórios"
-    if usuario == "admin" and senha == "1234":
-        return "Login realizado"
-    return "Credenciais inválidas"
